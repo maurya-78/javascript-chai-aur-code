24c108449fcc5687e9ea5644b451184bda1bb0e3
maurya-78 <jimaurya6386@gmail.com>
2025-12-09 16:51:28 +0530

    update

--- chai_and_code.js
+++ chai_and_code.js
@@ -945,8 +945,186 @@ coding.forEach( (val)=> {
 })
 
 function printMe(item){
-  console.log(item)
+ // console.log(item)
 }
 
 coding.forEach(printMe)
 
+coding.forEach( (item,index,arr) => {
+ // console.log(item,index,arr);
+
+})
+
+const myCoding = [
+  {
+    languageName:"JavaScript",
+    languageFileName:"js",
+  },
+  {
+    languageName:"Python",
+    languageFileName:"py",
+  },
+  {
+    languageName:"Ruby",
+    languageFileName:"rb",
+  },
+  {
+    languageName:"Java",
+    languageFileName:"java",
+  },
+  {
+    languageName:"C++",
+    languageFileName:"cpp",
+  },
+  {
+    languageName:"C",
+    languageFileName:"c",
+  
+  }
+]
+
+
+myCoding.forEach( (item) =>{
+
+   // console.log(item.languageName);
+   // console.log(item.languageFileName);
+})
+
+const values = coding.forEach( (item) => {
+  //console.log(item);
+ // return item;
+})
+//console.log(values);
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+//const newNums = myNums.filter( (num) => {  
+  // return num > 5
+//})
+//console.log(newNums);
+
+//  if we apply the arrow function then we use the curly brases without using the return statement then output will be empty array [].
+//  if we use the return statement then output will we provided.
+
+const newNums = []
+
+myNums.forEach( (num) => {
+  if(num > 5){
+    newNums.push(num);
+  }
+})
+//console.log(newNums);
+
+
+const books = [
+  {
+    title: "Book One", genre:"Fiction", publish: 1981,
+    edition: 2004
+  },
+  {
+    title: "Book Two", genre:"Non-Fiction", publish: 1992,
+    edition: 2008
+  },
+  {
+    title: "Book Three", genre:"History", publish: 1994,
+    edition: 2012
+  },
+  {
+    title: "Book Four", genre:"Non-Fiction", publish: 1996,
+    edition: 2010
+    },
+  {
+    title: "Book Five", genre:"Fiction", publish: 1998,
+    edition: 2015
+  },
+  {
+    title: "Book Six", genre:"History", publish: 2000,
+    edition: 2016
+  },
+  {
+    title: "Book Seven", genre:"Non-Fiction", publish: 2002,
+    edition: 2012
+  },
+  {
+    title: "Book Eight", genre:"History", publish: 2008,
+    edition: 2013
+  },
+  {
+    title: "Book Nine", genre:"Non-Fiction", publish: 2006,
+    edition: 2010
+  }
+];
+
+let userBooks = books.filter( (bk) => bk.genre === "History")
+
+//console.log(userBooks);   // filter -> compair the value (true or false)
+
+const userBooks1 = books.filter( (bk) =>  {return bk.publish >= 2000})
+const userBooks2 = books.filter( (bk) =>  {return (bk.publish >= 1994 && bk.publish <=2006)})
+//console.log(userBooks1);
+
+//console.log(userBooks1);
+
+
+const myNumbers = [4,8,12,16,20,24,28,32,36,40]
+
+const newNumbers = myNumbers.map( (num) => {return num +10} )
+//console.log(newNumbers);
+
+const chaining = myNumbers
+                 .map((num) => num*10)
+                 .map((num) => num+1)
+                 .map((num) => num/4)
+      
+//console.log(chaining);
+
+//  chaning method => in this method if we apply the map(method) in chain form then it will be executed 1st(with values) -> 2nd(with values of 1st chain) -> 3rd(with values of 2nd)
+// Example => [4,8,12, ....] -> 1.( 4 *10 -> 40+1 -> 41/4 => 10.25)
+// Example => [4,8,12, ... ] -> 2.( 8 *10 -> 80+1 -> 81/4 => 20.25)
+// Example => [4,8,12, ...] -> 3.( 12 *10 -> 120+1 -> 121/4 => 30.25)  and so on
+
+
+
+///     Reduse method 
+
+const myReduce = [1,2,3,4,5,6,7,8,9,10]
+
+const newReduce = myReduce.reduce( (acc, curr) => {
+  //console.log(`acc: ${acc} and curr: ${curr}`); 
+    return acc + curr
+} ,3)
+//console.log(newReduce);
+
+const myTotal = myReduce.reduce( (acc, curr) => {
+  //console.log(`acc: ${acc} and curr: ${curr}`); 
+    return acc + curr
+} ,0)
+//console.log(myTotal);
+
+const shopingCards = [
+  {
+    itemName:"javascript",
+    price:199,
+  },
+  {
+    itemName:"java",
+    price:299,
+  },
+  {
+    itemName:"python",
+    price:399,
+  },
+  {
+    itemName:"ruby",
+    price:499,
+  },
+  {
+    itemName:"c++",
+    price:599,
+  }
+]
+
+ const priceToPay = shopingCards.reduce((items,acc) => acc+ items.price,0)
+
+ console.log(priceToPay);
